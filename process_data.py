32e04ccdb2375c3f0bd30c010f6504d4322ad8b0
ngocphuong <mtnp912@gmail.com>
2025-05-09 21:00:00 +0700

    calculate-summary-stock

--- process_data.py
+++ process_data.py
@@ -1,9 +1,10 @@
 from openpyxl import load_workbook
-from datetime import date
+from datetime import date, datetime
 import pandas as pd
 import os
 from copy import copy
 
+# copy format cột trước đó
 def copy_cell_format(src_cell, tgt_cell):
     if src_cell.has_style:
         if src_cell.font: tgt_cell.font = copy(src_cell.font)
@@ -13,9 +14,12 @@ def copy_cell_format(src_cell, tgt_cell):
         if src_cell.protection: tgt_cell.protection = copy(src_cell.protection)
         if src_cell.alignment: tgt_cell.alignment = copy(src_cell.alignment)
 
+# hiện đang lấy file, insert 1 cột và lưu vào file mới cho dễ debug
 def update_report_file():
-    file_path = "INPUT DATA/INPUT DATA/output.report sample.xlsx"
+    file_path = "INPUT DATA/INPUT DATA.xlsx"
+    # file_path = "INPUT DATA/INPUT DATA/output_report_2025-05-08.xlsx"
     today = pd.Timestamp(date.today())
+    # today = pd.Timestamp(date.today()) - pd.Timedelta(days=1)
     today_str = today.strftime("%Y-%m-%d")
 
     # Tạo tên file mới
@@ -62,7 +66,36 @@ def update_report_file():
     wb.save(new_file)
     print(f"✅ Đã lưu file mới: {new_file}")
 
-def update_DL_to_HN_summary(file_path_input, file_path_report):
-    pass
 
-update_report_file()
+def calculate_summary_stock(file_path_input):
+    xls = pd.ExcelFile(file_path_input)
+    last_sheet_name = xls.sheet_names[-1]  # last sheet
+
+    # Đọc dữ liệu từ sheet cuối cùng, tên cột bắt đầu từ hàng t2
+    df = pd.read_excel(xls, sheet_name=last_sheet_name, header=1) # sai từ đoạn đọc file excel vậy trời
+
+    # Chuẩn hóa tên cột về chữ thường
+    df.columns = [str(col).lower() for col in df.columns]
+
+    # Làm sạch dữ liệu: loại bỏ dấu phẩy và chuyển tất cả các giá trị thành số nguyên
+    df = df.replace({',': ''}, regex=True)  # Loại bỏ dấu phẩy trong toàn bộ bảng
+    df = df.apply(pd.to_numeric, errors='coerce')  # Chuyển các giá trị thành số, lỗi sẽ trở thành NaN
+    df = df.dropna()  # Bỏ dòng có giá trị NaN
+
+    # Tìm cột 'thành tiền' (ignore case)
+    thanh_tien_col = next((col for col in df.columns if "thành tiền" in col.lower()), None)
+
+    if thanh_tien_col:
+        # Tính tổng
+        total_thanh_tien = df[thanh_tien_col].sum()
+        # parse ngày tháng năm theo tên sheet
+        current_year = date.today().year
+        full_date_str = f"{last_sheet_name}.{current_year}"
+        parsed_date = datetime.strptime(full_date_str, "%d.%m.%Y").date()
+        formatted_date = parsed_date.strftime("%d/%m/%Y")
+
+        print(total_thanh_tien, formatted_date)
+
+# update_report_file()
+# calculate_summary_stock("INPUT DATA/INPUT DATA/server.HN STOCK/TỒN KHO NGỌC THỤY THÁNG 04.2025.xlsx")
+calculate_summary_stock("INPUT DATA/INPUT DATA/server.SG STOCK/2025/THÁNG 4.xlsx")
